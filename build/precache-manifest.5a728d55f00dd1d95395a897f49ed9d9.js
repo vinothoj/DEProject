2473cce43470bf07fff0c9be3baad45fc21a34e1
vinoth <vinothoj19@gmail.com>
2021-02-17 17:09:19 +0530

    first build code

---
+++ build/precache-manifest.5a728d55f00dd1d95395a897f49ed9d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16135447d12243cdc778ab8fddeac46f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9679e85c5abaf980ca39",
+    "url": "/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "434b2e1f49b08154ce69",
+    "url": "/static/css/main.f6fd3d58.chunk.css"
+  },
+  {
+    "revision": "9679e85c5abaf980ca39",
+    "url": "/static/js/2.f290bcce.chunk.js"
+  },
+  {
+    "revision": "31dfda6d0f9d434c1f62fd251f373126",
+    "url": "/static/js/2.f290bcce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "434b2e1f49b08154ce69",
+    "url": "/static/js/main.f934d85a.chunk.js"
+  },
+  {
+    "revision": "7646288671f8458d76de",
+    "url": "/static/js/runtime-main.49627031.js"
+  }
+]);
